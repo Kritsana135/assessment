a6b2a8669b08375f223d30e24fc9dfea14027acd
kritsana (game) <kritsana.s@24slg.com>
2023-01-07 11:38:01 +0700

    add TestUpdateExpense repo

--- expense/repository/postgresql/postgresql_expense_int_test.go
+++ expense/repository/postgresql/postgresql_expense_int_test.go
@@ -1,6 +1,7 @@
 package postgresql_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/Kritsana135/assessment/domain"
@@ -36,6 +37,53 @@ func (suite *ExpenseRepoSuite) SetupTest() {
 func (suite *ExpenseRepoSuite) TearDownTest() {
 	err := suite.db.Migrator().DropTable(&domain.ExpenseTable{})
 	assert.NoError(suite.T(), err)
+}
+
+func (suite *ExpenseRepoSuite) TestGetExpenses() {
+	ctx := context.TODO()
+	expenses := []domain.ExpenseTable{
+		{
+			Amount: 100,
+		},
+		{
+			Amount: 200,
+		},
+		{
+			Amount: 300,
+		},
+	}
+
+	for _, expense := range expenses {
+		err := suite.expenseRepo.Create(ctx, &expense)
+		assert.NoError(suite.T(), err)
+	}
+
+	expensesFromRepo, err := suite.expenseRepo.GetExpenses(ctx)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), len(expenses), len(expensesFromRepo))
+}
+
+func (suite *ExpenseRepoSuite) TestUpdateExpense() {
+	ctx := context.TODO()
+	expense := domain.ExpenseTable{
+		Amount: 100,
+	}
+
+	err := suite.expenseRepo.Create(ctx, &expense)
+	assert.NoError(suite.T(), err)
+
+	expense.Amount = 200
+	err = suite.expenseRepo.UpdateExpense(ctx, uint64(expense.ID), &expense)
+	assert.NoError(suite.T(), err)
+
+	expenseFromRepo, err := suite.expenseRepo.GetExpensesById(ctx, uint64(expense.ID))
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), expense.Amount, expenseFromRepo.Amount)
+
+	// not found expense
+	err = suite.expenseRepo.UpdateExpense(ctx, 999, &expense)
+	assert.Error(suite.T(), err)
+	assert.Equal(suite.T(), gorm.ErrRecordNotFound, err)
 }
 
 func TestExpenseRepoSuite(t *testing.T) {
