cead4dacb748ddfb5734524f21d402e8d8b3e43e
kritsana (game) <kritsana.s@24slg.com>
2023-01-07 09:55:50 +0700

    add unit test in CreateExpense handler

--- expense/delivery/http_/expense_handler.go
+++ expense/delivery/http_/expense_handler.go
@@ -11,12 +11,12 @@ import (
 )
 
 type ExpenseHandler struct {
-	expUCase domain.ExpenseUseCase
+	ExpUCase domain.ExpenseUseCase
 }
 
 func NewExpenseHandler(r *gin.RouterGroup, expUCase domain.ExpenseUseCase) {
 	handler := &ExpenseHandler{
-		expUCase,
+		ExpUCase: expUCase,
 	}
 
 	er := r.Group("/expenses")
@@ -37,7 +37,7 @@ func (h *ExpenseHandler) CreateExpense(c *gin.Context) {
 		return
 	}
 
-	res, err := h.expUCase.CreateExpense(c.Request.Context(), body)
+	res, err := h.ExpUCase.CreateExpense(c.Request.Context(), body)
 	if err != nil {
 		logrus.Error(err)
 		c.JSON(apperrors.Status(err), domain.BaseResponse{Message: err.Error()})
@@ -48,7 +48,7 @@ func (h *ExpenseHandler) CreateExpense(c *gin.Context) {
 }
 
 func (h *ExpenseHandler) GetExpenses(ctx *gin.Context) {
-	res, err := h.expUCase.GetExpenses(ctx.Request.Context())
+	res, err := h.ExpUCase.GetExpenses(ctx.Request.Context())
 	if err != nil {
 		logrus.Error(err)
 		ctx.JSON(apperrors.Status(err), domain.BaseResponse{Message: err.Error()})
@@ -67,7 +67,7 @@ func (h *ExpenseHandler) GetExpensesById(ctx *gin.Context) {
 		return
 	}
 
-	res, err := h.expUCase.GetExpensesById(ctx.Request.Context(), uId)
+	res, err := h.ExpUCase.GetExpensesById(ctx.Request.Context(), uId)
 	if err != nil {
 		logrus.Error(err)
 		ctx.JSON(apperrors.Status(err), domain.BaseResponse{Message: err.Error()})
@@ -92,7 +92,7 @@ func (h *ExpenseHandler) UpdateExpense(ctx *gin.Context) {
 		return
 	}
 
-	res, err := h.expUCase.UpdateExpense(ctx.Request.Context(), uId, body)
+	res, err := h.ExpUCase.UpdateExpense(ctx.Request.Context(), uId, body)
 	if err != nil {
 		logrus.Error(err)
 		ctx.JSON(apperrors.Status(err), domain.BaseResponse{Message: err.Error()})
--- expense/delivery/http_/expense_handler_test.go
+++ expense/delivery/http_/expense_handler_test.go
@@ -1,18 +1,116 @@
 package http__test
 
 import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
 	"testing"
+
+	"github.com/Kritsana135/assessment/domain"
+	"github.com/Kritsana135/assessment/domain/apperrors"
+	"github.com/Kritsana135/assessment/domain/mocks"
+	"github.com/Kritsana135/assessment/expense/delivery/http_"
+	"github.com/Kritsana135/assessment/misc"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateExpense(t *testing.T) {
-	// t.Run("ce_1:the request body failed because the required field is missing.", func(t *testing.T) {
-	// 	body := `{
-	// 		"amount": 79,
-	// 		"note": "night market promotion discount 10 bath",
-	// 		"tags": ["food", "beverage"]
-	// 	}`
+	t.Run("ce_1:the request body failed because the required field is missing.", func(t *testing.T) {
+		bodies := []string{
+			`{
+				"amount": 79,
+				"note": "night market promotion discount 10 bath",
+				"tags": ["food", "beverage"]
+			}`,
+			`{
+				"title": "night market",
+				"note": "night market promotion discount 10 bath",
+				"tags": ["food", "beverage"]
+			}`,
+		}
 
-	// 	req, err := http.NewRequestWithContext(context.TODO(), http.MethodPost, "/article/"+strconv.Itoa(num), strings.NewReader(""))
+		for _, body := range bodies {
+			w := httptest.NewRecorder()
+			ctx := misc.GetTestGinContext(w)
+			ctx.Request.Header.Set("Content-Type", "application/json")
 
-	// })
+			ctx.Request.Body = io.NopCloser(bytes.NewBuffer([]byte(body)))
+
+			handler := http_.ExpenseHandler{}
+
+			handler.CreateExpense(ctx)
+
+			assert.Equal(t, http.StatusBadRequest, w.Code)
+		}
+	})
+
+	t.Run("ce_2:should get error when create", func(t *testing.T) {
+		body := `{
+				  "title": "night market",
+				  "amount": 79,
+				  "note": "night market promotion discount 10 bath",
+				  "tags": ["food", "beverage"]
+			     }`
+
+		w := httptest.NewRecorder()
+		ctx := misc.GetTestGinContext(w)
+		ctx.Request.Header.Set("Content-Type", "application/json")
+
+		ctx.Request.Body = io.NopCloser(bytes.NewBuffer([]byte(body)))
+
+		mockExpenseUseCase := mocks.NewExpenseUseCase(t)
+		parseBody := domain.CrateExpenseReq{
+			Title:  "night market",
+			Amount: 79,
+			Note:   "night market promotion discount 10 bath",
+			Tags:   []string{"food", "beverage"},
+		}
+		mockExpenseUseCase.On("CreateExpense", ctx.Request.Context(), parseBody).
+			Return(domain.ExpenseTable{}, apperrors.NewInternal())
+		handler := http_.ExpenseHandler{
+			ExpUCase: mockExpenseUseCase,
+		}
+
+		handler.CreateExpense(ctx)
+
+		assert.Equal(t, http.StatusInternalServerError, w.Code)
+	})
+
+	t.Run("ce_3:should get 201 status", func(t *testing.T) {
+		body := `{
+				  "title": "night market",
+				  "amount": 79,
+				  "note": "night market promotion discount 10 bath",
+				  "tags": ["food", "beverage"]
+			     }`
+
+		w := httptest.NewRecorder()
+		ctx := misc.GetTestGinContext(w)
+		ctx.Request.Header.Set("Content-Type", "application/json")
+
+		ctx.Request.Body = io.NopCloser(bytes.NewBuffer([]byte(body)))
+
+		mockExpenseUseCase := mocks.NewExpenseUseCase(t)
+		parseBody := domain.CrateExpenseReq{
+			Title:  "night market",
+			Amount: 79,
+			Note:   "night market promotion discount 10 bath",
+			Tags:   []string{"food", "beverage"},
+		}
+		mockExpenseUseCase.On("CreateExpense", ctx.Request.Context(), parseBody).
+			Return(domain.ExpenseTable{Title: "night market"}, nil)
+		handler := http_.ExpenseHandler{
+			ExpUCase: mockExpenseUseCase,
+		}
+
+		handler.CreateExpense(ctx)
+
+		var res domain.ExpenseTable
+		json.NewDecoder(w.Body).Decode(&res)
+
+		assert.Equal(t, http.StatusCreated, w.Code)
+		assert.Equal(t, "night market", res.Title)
+	})
 }
